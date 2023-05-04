a1ddde748c1668cfac271c712fd01161764d7c18
Magar15 <magalhaesg139@gmail.com>
2023-05-03 21:47:59 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/magar/curso/entities/Product.java
+++ src/main/java/com/magar/curso/entities/Product.java
@@ -9,27 +9,34 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	@Id 
+
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String description;
 	private Double price;
 	private String imgUrl;
+
+	// comando para garantir que estará vazia e não nula porém instaciada, Set é
+	// interface o Hast seria a "classe"
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", 
+	joinColumns = @JoinColumn(name = "product_id"), 
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	
-	//comando para garantir que estará vazia e não nula porém instaciada, Set é interface o Hast seria a "classe"
-	@Transient
 	private Set<Category> categories = new HashSet<>();
-	
+
 	public Product() {
 	}
 
@@ -102,8 +109,5 @@ public class Product implements Serializable {
 		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
+
 }
