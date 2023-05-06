12d623111ac4b80bc56e629b7671c6116fadc672
Magar15 <magalhaesg139@gmail.com>
2023-05-05 22:27:54 -0300

    Payment, one-to-one association

--- src/main/java/com/magar/curso/config/TestConfig.java
+++ src/main/java/com/magar/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.magar.curso.entities.Category;
 import com.magar.curso.entities.Order;
 import com.magar.curso.entities.OrderItem;
+import com.magar.curso.entities.Payment;
 import com.magar.curso.entities.Product;
 import com.magar.curso.entities.User;
 import com.magar.curso.entities.enums.OrderStatus;
@@ -87,8 +88,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1); //associação do pedido o1
+		
+		orderRepository.save(o1);
 	
 	}
-	
-
 }
